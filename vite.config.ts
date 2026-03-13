3cb6cfdb324e4b63a1f51b9ffe12f5a1af43b85a
nive-dev <nivethithakesavan7823@gmail.com>
2026-03-13 12:48:23 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  base:"/",
+  base: "/Hrify-Interior/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
